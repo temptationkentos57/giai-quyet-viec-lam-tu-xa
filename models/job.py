b6115ff0a4e245ca535f43c898b0623b8cdcbb75
temptationkentos57 <kreetlannoo@hotmail.com>
2025-10-03 18:17:46 +0200

    feat: initial commit for models/job.py

---
+++ models/job.py
@@ -0,0 +1,7 @@
+class Job:
+    def __init__(self, title, description, company, location, salary):
+        self.title = title
+        self.description = description
+        self.company = company
+        self.location = location
+        self.salary = salary
